73ddab2bcfd8899dfa2ae4123537dc7ac09d7b1e
Harshadi <46489854+Harshadi@users.noreply.github.com>
2020-12-03 01:12:36 +0530

    Add files via upload

---
+++ src/components/Titles.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const Titles=()=>(
+
+    <div>
+<h1  className="title-container__title">Weather Forecast</h1>
+<p className="title-container__subtitle">Find out Temperature, Conditions and more...</p>
+</div>
+    
+);
+
+export default Titles;
